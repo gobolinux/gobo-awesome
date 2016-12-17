0c4de680091a696ba82e52cb0f3760a1d46321ac
Hisham <hisham@gobolinux.org>
2016-12-17 18:16:15 -0200

    Flicker-free corner keys.

--- gobo/awesome/docking.lua
+++ gobo/awesome/docking.lua
@@ -89,8 +89,8 @@ local function get_corner_fn(bottom, right)
 end
 
 function docking.corner_key(bottom, right, mods, key)
-   local fn = get_corner_fn(bottom, right)
-   return awful.key(mods, key, fn, { description = "Arrange at "..bottom.."-"..right.." corner", group = "awesome gobolinux" })
+   local corner_fn = get_corner_fn(bottom, right)
+   return awful.key(mods, key, corner_fn, { description = "Arrange at "..bottom.."-"..right.." corner", group = "awesome gobolinux" })
 end
 
 local cornering = {
@@ -99,166 +99,167 @@ local cornering = {
    timer = nil
 }
 
-local function check_corner(c, bottom, right)
+local function check_corner(c, bottom, right, non_corner_fn)
    if cornering.timer then
       if (cornering.bottom and not bottom) or (cornering.right and not right) then
          cornering.timer:stop()
-         local fn = get_corner_fn(cornering.bottom or bottom, cornering.right or right)
+         local corner_fn = get_corner_fn(cornering.bottom or bottom, cornering.right or right)
          cornering.timer = nil
          cornering.right = nil
          cornering.bottom = nil
-         fn(c)
+         corner_fn(c)
          return true
       end
    else
-      cornering.timer = timer({timeout = 0.2})
+      cornering.x = c.x
+      cornering.y = c.y
+      local my_timer = timer({timeout = 0.05})
+      cornering.timer = my_timer
       cornering.right = right
       cornering.bottom = bottom
-      cornering.timer:connect_signal("timeout", function()
+      my_timer:connect_signal("timeout", function()
+         my_timer:stop()
          cornering.timer = nil
          cornering.right = nil
          cornering.bottom = nil
+         non_corner_fn()
       end)
       cornering.timer:start()
    end
 end
 
 function docking.dock_left(c)
-   if check_corner(c, nil, "left") then
-      return
-   end
-   local curr = c:geometry()
-   local area = screen[c.screen].workarea
-   local s_area = area
-   local half = math.floor(area.width / 2)
-   local offset = 0
-   local mode = "left"
-   local maxd = c.maximized
-   if (maxd or (curr.x == area.x and curr.y == area.y)) and area.x > 0 then
-      local nextscreen = awful.screen.getbycoord(area.x - 1, area.y)
-      area = screen[nextscreen].workarea
-      if maxd then
-         half = area.width
-         offset = 0
-         mode = "up"
-         c.screen = nextscreen
+   check_corner(c, nil, "left", function()
+      local curr = c:geometry()
+      local area = screen[c.screen].workarea
+      local s_area = area
+      local half = math.floor(area.width / 2)
+      local offset = 0
+      local mode = "left"
+      local maxd = c.maximized
+      if (maxd or (curr.x == area.x and curr.y == area.y)) and area.x > 0 then
+         local nextscreen = awful.screen.getbycoord(area.x - 1, area.y)
+         area = screen[nextscreen].workarea
+         if maxd then
+            half = area.width
+            offset = 0
+            mode = "up"
+            c.screen = nextscreen
+         else
+            half = math.floor(area.width / 2)
+            offset = half
+            mode = "right"
+         end
       else
-         half = math.floor(area.width / 2)
-         offset = half
-         mode = "right"
+         c.maximized = false
       end
-   else
-      c.maximized = false
-   end
-   c:geometry({ x = area.x + offset,
-      y = area.y,
-      width = half,
-      height = area.height
-   })
-   save_relative_geometry(c, curr, s_area)
-   auto_tile[c].mode = mode
-   c.border_width = 0
+      c:geometry({ x = area.x + offset,
+         y = area.y,
+         width = half,
+         height = area.height
+      })
+      save_relative_geometry(c, curr, s_area)
+      auto_tile[c].mode = mode
+      c.border_width = 0
+   end)
 end
 
 function docking.dock_right(c)
-   if check_corner(c, nil, "right") then
-      return
-   end
-   local area = screen[c.screen].workarea
-   local s_area = area
-   local half = math.floor(area.width / 2)
-   local curr = c:geometry()
-   local offset = 0
-   local mode = "right"
-   local nextscreen = awful.screen.getbycoord(area.x + area.width, area.y, -1)
-   local maxd = c.maximized
-   if (maxd or (curr.x == area.x + half and curr.y == area.y)) and nextscreen ~= -1 then
-      area = screen[nextscreen].workarea
-      if maxd then
-         half = area.width
-         offset = area.width
-         mode = "up"
-         c.screen = nextscreen
+   check_corner(c, nil, "right", function()
+      local area = screen[c.screen].workarea
+      local s_area = area
+      local half = math.floor(area.width / 2)
+      local curr = c:geometry()
+      local offset = 0
+      local mode = "right"
+      local nextscreen = awful.screen.getbycoord(area.x + area.width, area.y, -1)
+      local maxd = c.maximized
+      if (maxd or (curr.x == area.x + half and curr.y == area.y)) and nextscreen ~= -1 then
+         area = screen[nextscreen].workarea
+         if maxd then
+            half = area.width
+            offset = area.width
+            mode = "up"
+            c.screen = nextscreen
+         else
+            half = math.floor(area.width / 2)
+            offset = half
+            mode = "left"
+         end
       else
-         half = math.floor(area.width / 2)
-         offset = half
-         mode = "left"
+         c.maximized = false
       end
-   else
-      c.maximized = false
-   end
-   c:geometry({ x = area.x + half - offset,
-      y = area.y,
-      width = half,
-      height = area.height
-   })
-   save_relative_geometry(c, curr, s_area)
-   auto_tile[c].mode = mode
-   c.border_width = 0
+      c:geometry({ x = area.x + half - offset,
+         y = area.y,
+         width = half,
+         height = area.height
+      })
+      save_relative_geometry(c, curr, s_area)
+      auto_tile[c].mode = mode
+      c.border_width = 0
+   end)
 end
 
 function docking.dock_up(c)
-   if check_corner(c, "top", nil) then
-      return
-   end
-   local area = screen[c.screen].workarea
-   local half = math.floor(area.height / 2)
-   local curr = c:geometry()
-   local tophalf = (curr.x == area.x and curr.y == area.y and math.abs(curr.height - half) < 20)
-   if c.maximized or (not tophalf) then
-      c.maximized = false
-      c:geometry({ x = area.x,
-         y = area.y,
-         width = area.width,
-         height = half,
-      })
-   elseif tophalf then
-      c.maximized = true
-      c:geometry({ x = area.x,
-         y = area.y,
-         width = area.width,
-         height = area.height,
-      })
-   end
-   save_relative_geometry(c, curr, area)
-   auto_tile[c].mode = "up"
-   c.border_width = 0
+   check_corner(c, "top", nil, function()
+      local area = screen[c.screen].workarea
+      local half = math.floor(area.height / 2)
+      local curr = c:geometry()
+      local tophalf = (curr.x == area.x and curr.y == area.y and math.abs(curr.height - half) < 20)
+      if c.maximized or (not tophalf) then
+         c.maximized = false
+         c:geometry({ x = area.x,
+            y = area.y,
+            width = area.width,
+            height = half,
+         })
+      elseif tophalf then
+         c.maximized = true
+         c:geometry({ x = area.x,
+            y = area.y,
+            width = area.width,
+            height = area.height,
+         })
+      end
+      save_relative_geometry(c, curr, area)
+      auto_tile[c].mode = "up"
+      c.border_width = 0
+   end)
 end
 
 function docking.dock_down(c)
-   if check_corner(c, "bottom", nil) then
-      return
-   end
-   c.maximized = false
-   local area = screen[c.screen].workarea
-   local half = math.floor(area.height / 2)
-   local curr = c:geometry()
-   if curr.y ~= area.y + half then
-      c:geometry({ x = area.x,
-         y = area.y + half,
-         width = area.width,
-         height = half,
-      })
-      save_relative_geometry(c, curr, area)
-      auto_tile[c].mode = "down"
-      c.border_width = 0
-   elseif auto_tile[c] then
-      local old = auto_tile[c].old
-      if old.x == 0 and old.y == 0 then
-         old = {
-            x = area.x + (area.width / 4),
-            y = area.y + (area.height / 4),
-            width = area.width / 2,
-            height = area.height / 2,
-         }
-      else
-         old.x = old.x + area.x
-         old.y = old.y + area.y
+   check_corner(c, "bottom", nil, function()
+      c.maximized = false
+      local area = screen[c.screen].workarea
+      local half = math.floor(area.height / 2)
+      local curr = c:geometry()
+      if curr.y ~= area.y + half then
+         c:geometry({ x = area.x,
+            y = area.y + half,
+            width = area.width,
+            height = half,
+         })
+         save_relative_geometry(c, curr, area)
+         auto_tile[c].mode = "down"
+         c.border_width = 0
+      elseif auto_tile[c] then
+         local old = auto_tile[c].old
+         if old.x == 0 and old.y == 0 then
+            old = {
+               x = area.x + (area.width / 4),
+               y = area.y + (area.height / 4),
+               width = area.width / 2,
+               height = area.height / 2,
+            }
+         else
+            old.x = old.x + area.x
+            old.y = old.y + area.y
+         end
+         c:geometry(old)
+         awful.placement.no_offscreen(c)
+         undock_auto_tile(c)
       end
-      c:geometry(old)
-      awful.placement.no_offscreen(c)
-      undock_auto_tile(c)
-   end
+   end)
 end
 
 function docking.smart_mouse_move(c)
