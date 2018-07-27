fe52e5fabb7d78520d970d9aac785389c3d74759
Hisham Muhammad <hisham@gobolinux.org>
2018-07-27 10:46:58 -0300

    add "Send to virtual desktop" options to window title context menu

--- gobo/awesome/window_menu.lua
+++ gobo/awesome/window_menu.lua
@@ -17,6 +17,19 @@ end
 function window_menu.show(c, menu_opts)
    window_menu.hide(c)
    local entries = {}
+   local my_tags = c:tags()
+
+   local s_tags = {}
+   local in_tag = {}
+   for i = 1, 4 do
+      s_tags[i] = awful.tag.gettags(client.focus.screen)[i]
+      for _, t in ipairs(my_tags) do
+         if t == s_tags[i] then
+            in_tag[i] = true
+            break
+         end
+      end
+   end
    
    if c.maximized then
       table.insert(entries, {
@@ -44,6 +57,31 @@ function window_menu.show(c, menu_opts)
       end,
       c.ontop and beautiful.check_icon or nil})
    table.insert(entries, {
+      "Send to...", {
+         {
+            "Virtual desktop 1", function()
+               awful.client.movetotag(s_tags[1])
+            end,
+            in_tag[1] and beautiful.check_icon or nil,
+         },{
+            "Virtual desktop 2", function()
+               awful.client.movetotag(s_tags[2])
+            end,
+            in_tag[2] and beautiful.check_icon or nil,
+         },{
+            "Virtual desktop 3", function()
+               awful.client.movetotag(s_tags[3])
+            end,
+            in_tag[3] and beautiful.check_icon or nil,
+         },{
+            "Virtual desktop 4", function()
+               awful.client.movetotag(s_tags[4])
+            end,
+            in_tag[4] and beautiful.check_icon or nil,
+         }
+      }
+   })
+   table.insert(entries, {
       "Full screen", function()
          c.fullscreen = not c.fullscreen
       end,
@@ -54,7 +92,7 @@ function window_menu.show(c, menu_opts)
       end, 
       beautiful.titlebar_close_button_focus })
 
-   entries.theme = { height = 24, width = 150 }
+   entries.theme = { height = 24, width = 170 }
    local menu = awful.menu.new(entries)
    menu:show(menu_opts)
    window_menus[c] = menu
